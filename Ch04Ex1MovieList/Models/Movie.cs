47f608829dd106b87e2671439dd569f61b3d8001
JKirkis <jordkerkvliet@gmail.com>
2023-10-01 22:57:16 -0500

    Step 12

---
+++ Ch04Ex1MovieList/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ch04Ex1MovieList.Models
+{
+    public class Movie
+    {
+        // EF Core will configure the database to generate this value
+        public int MovieId { get; set; }
+
+        [Required(ErrorMessage = "Please eneter a name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a year.")]
+        [Range(1889, 2999, ErrorMessage = "Year must be after 1889.")]
+        public int? Year{ get; set; }
+
+        [Required(ErrorMessage = "Please enter a rating.")]
+        [Range(1, 5, ErrorMessage = "Rating must be ebtween 1 and 5.")]
+        public int? Rating { get; set; }
+    }
+}
